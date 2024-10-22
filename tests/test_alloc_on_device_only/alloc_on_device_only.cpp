33b8f838de32aee4ef4445a7453ba09d9db9034a
J S <jakob.schaeffeler@tum.de>
2024-10-22 10:46:25 +0200

    add alloc on device only test

--- tests/test_alloc_on_device_only/alloc_on_device_only.cpp
+++ tests/test_alloc_on_device_only/alloc_on_device_only.cpp
@@ -8,7 +8,7 @@ int main() {
     double* array; //= (double*) aligned_alloc(64, sizeof(double)*N);
     double* result; //= (double*) aligned_alloc(64, sizeof(double)*N);
     // Allocate memory on the device for array and result
-    #pragma omp target enter data map(alloc: array[0:N], result[0:N])
+    #pragma omp target enter data map(alloc: array[0:SIZE], result[0:SIZE])
 
     // Initialize the array directly on the device
     #pragma omp target teams distribute parallel for
@@ -17,16 +17,16 @@ int main() {
     }
     // Offload the computation to the GPU
     #pragma omp target teams distribute parallel for 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < SIZE; i++) {
         // Each thread accesses a consecutive element of the array
         result[i] = array[i] * 2;
     }
     
     #pragma omp target teams distribute parallel for 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < SIZE; i++) {
         if (result[i] != array[i] * 2) {
             printf("Error at index %d\n", i);
         }
     }
-    #pragma omp target exit data map(release: array[0:N], result[0:N])
+    #pragma omp target exit data map(release: array[0:SIZE], result[0:SIZE])
 }
--- tests/test_alloc_on_device_only/compile.sh
+++ tests/test_alloc_on_device_only/compile.sh
@@ -10,10 +10,15 @@ COMPILER=$1
 ARCH=$2
 FLAGS=""
 
-make test_alloc_on_device_only_O0 CC=$COMPILER GPU_ARCH=$ARCH
-make test_alloc_on_device_only_O1 CC=$COMPILER GPU_ARCH=$ARCH
-make test_alloc_on_device_only_O2 CC=$COMPILER GPU_ARCH=$ARCH
-make test_alloc_on_device_only_O3 CC=$COMPILER GPU_ARCH=$ARCH
+if [ ! -z "$3" ]; then
+  FLAGS=$3
+fi
+
+
+make test_alloc_on_device_only_O0 CC=$COMPILER GPU_ARCH=$ARCH CXXFLAGS_COMPILER="$FLAGS"
+make test_alloc_on_device_only_O1 CC=$COMPILER GPU_ARCH=$ARCH CXXFLAGS_COMPILER="$FLAGS"
+make test_alloc_on_device_only_O2 CC=$COMPILER GPU_ARCH=$ARCH CXXFLAGS_COMPILER="$FLAGS"
+make test_alloc_on_device_only_O3 CC=$COMPILER GPU_ARCH=$ARCH CXXFLAGS_COMPILER="$FLAGS"
 
 
 
